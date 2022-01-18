7040de396d96db75fb397af5ceb569eda3a219ad
Kirill Mot <kirill.motrichkin@ya.ru>
2022-01-18 22:12:10 +0300

    Added memory info to status

--- src/main/java/com/github/superkiria/cbbot/actions/actors/StatusActor.java
+++ src/main/java/com/github/superkiria/cbbot/actions/actors/StatusActor.java
@@ -31,6 +31,7 @@ public class StatusActor implements ChatActor {
             return;
         }
         context.setResponse("Status:\n"
+                + (Runtime.getRuntime().totalMemory() / 1024 / 1024) + " / " + (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "\n"
                 + messageQueue.size() + "\n"
                 + subscriptionManager.getCurrentSubscription() + "\n"
                 + subscriptionManager.bestRoundToSubscribe() + "\n"
